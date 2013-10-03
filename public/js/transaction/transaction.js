8dc733baa9f0e40947c7a8619e9d64fddb7c84d4
Sergey <timeout2x@gmail.com>
2013-10-04 01:11:01 +0200

    fixed #1

--- public/js/transaction/transaction.js
+++ public/js/transaction/transaction.js
@@ -48,13 +48,14 @@ angular.module('expence.transaction', ['ngResource', 'ui.router', 'expence.proje
     }
     
     $scope.batch = function() {
-      var i=0, min = 10, max = 1000,
+      var k=0, min = 10, max = 1000, i=0,
       count = parseInt($scope.transaction.description);
       create();
       
       function create() {
         var transaction = new Transaction($scope.transaction);
         
+        transaction.description = loremIpsum(); 
         transaction.amount = (Math.random() * (max - min + 1)) + min; 
         transaction.type = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
         transaction.account = theProject.accounts[Math.floor(Math.random()*theProject.accounts.length)]._id; 
@@ -65,14 +66,32 @@ angular.module('expence.transaction', ['ngResource', 'ui.router', 'expence.proje
           $scope.paginate();
           theProject.transactionWatch++;
           
-          i++;
-          if (i >= count) {
+          k++;
+          console.log(k, count);
+          if (k >= count) {
             
           }
           else {
             create();
           }
         });
+      }
+
+      function loremIpsum() {
+        var loremIpsumWordBank = new Array("lorem","ipsum","dolor","sit","amet,","consectetur","adipisicing","elit,","sed","do","eiusmod","tempor","incididunt","ut","labore","et","dolore","magna","aliqua.","enim","ad","minim","veniam,","quis","nostrud","exercitation","ullamco","laboris","nisi","ut","aliquip","ex","ea","commodo","consequat.","duis","aute","irure","dolor","in","reprehenderit","in","voluptate","velit","esse","cillum","dolore","eu","fugiat","nulla","pariatur.","excepteur","sint","occaecat","cupidatat","non","proident,","sunt","in","culpa","qui","officia","deserunt","mollit","anim","id","est","laborum.","sed","ut","perspiciatis,","unde","omnis","iste","natus","error","sit","voluptatem","accusantium","doloremque","laudantium,","totam","rem","aperiam","eaque","ipsa,","quae","ab","illo","inventore","veritatis","et","quasi","architecto","beatae","vitae","dicta","sunt,","explicabo.","nemo","enim","ipsam","voluptatem,","quia","voluptas","sit,","aspernatur","aut","odit","aut","fugit,","sed","quia","consequuntur","magni","dolores","eos,","qui","ratione","voluptatem","sequi","nesciunt,","neque","porro","quisquam","est,","qui","dolorem","ipsum,","quia","dolor","sit,","amet,","consectetur,","adipisci","velit,","sed","quia","non","numquam","eius","modi","tempora","incidunt,","ut","labore","et","dolore","magnam","aliquam","quaerat","voluptatem.","ut","enim","ad","minima","veniam,","quis","nostrum","exercitationem","ullam","corporis","suscipit","laboriosam,","nisi","ut","aliquid","ex","ea","commodi","consequatur?","quis","autem","vel","eum","iure","reprehenderit,","qui","in","ea","voluptate","velit","esse,","quam","nihil","molestiae","consequatur,","vel","illum,","qui","dolorem","eum","fugiat,","quo","voluptas","nulla","pariatur?","at","vero","eos","et","accusamus","et","iusto","odio","dignissimos","ducimus,","qui","blanditiis","praesentium","voluptatum","deleniti","atque","corrupti,","quos","dolores","et","quas","molestias","excepturi","sint,","obcaecati","cupiditate","non","provident,","similique","sunt","in","culpa,","qui","officia","deserunt","mollitia","animi,","id","est","laborum","et","dolorum","fuga.","harum","quidem","rerum","facilis","est","et","expedita","distinctio.","Nam","libero","tempore,","cum","soluta","nobis","est","eligendi","optio,","cumque","nihil","impedit,","quo","minus","id,","quod","maxime","placeat,","facere","possimus,","omnis","voluptas","assumenda","est,","omnis","dolor","repellendus.","temporibus","autem","quibusdam","aut","officiis","debitis","aut","rerum","necessitatibus","saepe","eveniet,","ut","et","voluptates","repudiandae","sint","molestiae","non","recusandae.","itaque","earum","rerum","hic","tenetur","a","sapiente","delectus,","aut","reiciendis","voluptatibus","maiores","alias","consequatur","aut","perferendis","doloribus","asperiores","repellat");
+        var minWordCount = 2;
+        var maxWordCount = 7;
+
+        var randy = Math.floor(Math.random()*(maxWordCount - minWordCount)) + minWordCount;
+        var ret = "";
+        for(i = 0; i < randy; i++) {
+          var newTxt = loremIpsumWordBank[Math.floor(Math.random() * (loremIpsumWordBank.length - 1))];
+          if (ret.substring(ret.length-1,ret.length) == "." || ret.substring(ret.length-1,ret.length) == "?") {
+            newTxt = newTxt.substring(0,1).toUpperCase() + newTxt.substring(1, newTxt.length);
+          }
+          ret += " " + newTxt;
+        }
+        return ret;
       }
     }
     
